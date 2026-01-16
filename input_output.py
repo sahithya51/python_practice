4312780d2bfd1064da786ac192a96fa11140ca0b
Your Name <bsahithya51@gmail.com>
2026-01-16 21:57:49 +0530

    Day1 - Python_basics

---
+++ input_output.py
@@ -0,0 +1,6 @@
+#program to take input from user and print output
+name = input("Enter your name:")
+reg_num = input("Enter your registration number:")
+print("Hello",name)
+print("Registration number:",reg_num)
+
---
+++ operation.py
@@ -0,0 +1,14 @@
+#python program to take 2 numbers as input and do different operations and print result value
+num1 = int(input("Enter 1st number:"))
+num2 = int(input("Enter 2st number:"))
+
+sum          = num1 + num2
+Diffirence   = num1 - num2
+Product      = num1*num2
+quotient     = num1/num2
+Remainder    = num1 %num2
+print(sum)
+print(Diffirence )
+print(Product)
+print(quotient)
+print(Remainder)
